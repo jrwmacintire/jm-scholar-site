3a81933f947a3a4fca338e88e266486306cfaabf
John MacIntire <jrwmacintire@gmail.com>
2018-10-29 14:58:35 -0700

    Initial commit. Basic server function, such as Express implementation and static file serving are functional.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files, such as main index.html page.
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//     res.send("This server receives post requests at '/register-student' and '/register-school'");
+// });
+
+app.post('/register-student', (req, res) => {
+    res.send("Testing POST requests at '/register-student'.");
+});
+
+app.post('/register-school', (req, res) => {
+    res.send("Testing POST requests at '/register-school'.");
+});
+
+app.listen(port, () => console.log(`Now listening on port: ${port}`));
