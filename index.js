708206ef500f0979a779362c303a06b933dedad8
John MacIntire <jrwmacintire@gmail.com>
2018-12-08 21:52:07 +0000

    Added schema for Student Application Form submissions to mLab

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const mongoose = require('mongoose');
 
 // init database
 mongoose.Promise = global.Promise;
-const formSchema = new mongoose.Schema({
+
+// High School Request Form Schema
+const hsReqFormSchema = new mongoose.Schema({
     name: String,
     email: String,
     phone: String,
@@ -25,7 +27,30 @@ const formSchema = new mongoose.Schema({
     addContactEmail2: String,
     addContactPhone2: String
 });
-const RegForm = mongoose.model('RegForm', formSchema);
+const HSReqForm = mongoose.model('HSReqForm', hsReqFormSchema);
+
+// Student Application Form Schema
+const studentFormSchema = new mongoose.Schema({
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    email: String,
+    phoneNumber: String,
+    birthYear: String,
+    seniorStatus: String,
+    usCitizen: String,
+    schoolName: String,
+    schoolCode: String,
+    schoolAddress: String,
+    schoolCity: String,
+    schoolState: String,
+    attendsParticipatingSchool: String
+});
+const StudentForm = mongoose.model('StudentForm', studentFormSchema);
+
+// Volunteer to Judge Form Schema
+
+// Connect to database
 mongoose.connect('mongodb://John:jefferson1776@ds261917.mlab.com:61917/jm-scholar-uploads', { useNewUrlParser: true });
 
 // init middleware
@@ -40,12 +65,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
-// @route POST '/submit-reg-form'
+// @route POST '/submit-hs-req-form'
 // @desc Intercepts POST requests from school registration form
-app.post('/submit-reg-form', (req, res) => {
-    console.log(`Received POST!`);
+app.post('/submit-hs-req-form', (req, res) => {
+    console.log(`HS Request Form has been submitted!`);
     // res.json(req.body);
-    const data = new RegForm(req.body);
+    const data = new HSReqForm(req.body);
+    data.save()
+        .then(item => res.send('Item saved to database!'))
+        .catch(err => res.status(400).send('Unable to save to database.'));
+});
+
+// @route POST '/submit-student-app'
+// @desc Intercepts POST requests from student application form
+app.post('/submit-student-app', (req, res) => {
+    console.log(`Student Application Form submitted!`);
+    // res.json(req.body);
+    const data = new StudentForm(req.body);
     data.save()
         .then(item => res.send('Item saved to database!'))
         .catch(err => res.status(400).send('Unable to save to database.'));
